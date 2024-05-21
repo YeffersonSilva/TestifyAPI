18a3afeaa05e4dcef27f2e1821bdf89a84d36876
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-21 15:40:54 -0300

    chore: Add nodemon and express dependencies

--- src/server.js
+++ src/server.js
@@ -1,1 +1,9 @@
-const hellos = 'Hellos'
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    })
