7b7284bb9dfc36da917c6405c6d58d0e17921ad6
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-23 14:38:05 -0300

    feat: Add user creation endpoint to app.js

--- src/app.js
+++ src/app.js
@@ -6,5 +6,18 @@ const pino = require('pino-http')()
 const app = express()
 
 app.use(pino)
+app.use(express.json())
+
+const router = express.Router()
+
+router.post('/users', (req, res) => {
+    const { name, adress, age } = req.body
+    
+    users.push({ name, adress, age } )
+
+   return res.status(201).send(users)
+    })
+
+app.use(router)
 
 module.exports = app
