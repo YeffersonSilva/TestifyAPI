d9dd09e9a5ec0b17eea14b63c9c0cd92d0809526
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-23 16:00:29 -0300

    chore: Update server.js to use ES modules and add user and product routes

--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
-const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv';
+import { router as userRouter } from './routes/users.js';
+import { router as productRouter } from './routes/products.js';
+
+dotenv.config();
+
 const app = express();
-require('dotenv').config();
-const users = require('./routes/users');
 
+// Middleware to parse JSON bodies
 app.use(express.json());
-app.use('/users', users.router);
 
+// Use the users and products routes
+app.use('/users', userRouter);
+app.use('/products', productRouter);
+
+// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-module.exports = app;
+export default app;
